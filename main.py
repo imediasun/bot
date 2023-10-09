00ffbcc8822e6c51ec5a2f589018958bd84097a3
Daniylllll <143342450+Daniylllll@users.noreply.github.com>
2023-10-09 16:53:12 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,66 @@
-from aiogram import Dispatcher, Bot
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from config import TOKEN_API
+import asyncio
+import multiprocessing
+import disnake
+from aiogram import Dispatcher, Bot, executor, types
+from disnake.ext import commands as commands_ds
 
-storage = MemoryStorage()
+from config import *
 
-bot = Bot(TOKEN_API)
-dp = Dispatcher(bot=bot, storage=storage)
+bot = Bot(TG_TOKEN)
+dp = Dispatcher(bot=bot)
+
+intents = disnake.Intents.all()
+bot_ds = commands_ds.Bot(command_prefix="/", intents=intents)
+
+
+async def send_ds_mess(channel_id, mess):
+    channel = bot_ds.get_channel(channel_id)
+    print(channel)
+    await channel.send(mess)
+
+
+@bot_ds.event
+async def on_ready():
+    # await send_ds_mess(1159239010930130976, "hello world")
+    print(f'Logged in as {bot_ds.user.name}')
+    await send_ds_mess(1160219846571733042, "jjj")
+
+
+@dp.message_handler(commands=['start'])
+async def cd_start(message: types.Message):
+    await message.answer("Привет друг!")
+    channel = bot_ds.get_channel(1160219846571733042)
+    print(channel)
+    await channel.send("hello world")
+
+
+async def send_mess():
+    await bot.send_message(chat_id=1810112650,
+                           text="I`m here!")
+
+
+@bot_ds.command()
+async def start(ctx):
+    await ctx.send("hi!")
+    await send_mess()
+    await send_ds_mess(1159239010930130976, "hello world")
+
+
+def run_tg_bot():
+    executor.start_polling(dp,
+                           skip_updates=True)
+
+
+def run_ds_bot():
+    bot_ds.run(token=DS_TOKEN)
+
+
+if __name__ == "__main__":
+    discord_process = multiprocessing.Process(target=run_ds_bot)
+    telegram_process = multiprocessing.Process(target=run_tg_bot)
+
+    discord_process.start()
+    telegram_process.start()
+
+    discord_process.join()
+    telegram_process.join()
